a83d301b48ba8677e6a854a82a11f66d5c41c421
Brian Ruiz <brianruiz123@yahoo.com>
2020-01-20 12:38:27 -0600

    added file to grab data

---
+++ process/getdata.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_data():
+    testdata = pd.read_csv('data/testdata.csv')
+    testdata.dropna()
+    
+    # renaming the columns
+    testdata.rename(columns={'LOCATION Latitude : ':'Latitude'
+                        'LOCATION Longitude : ':'Longitude'
+                        'LOCATION Altitude ( m)':'Altitude (m)'}, inplace=True)
+    
+    return testdata
+    
--- process/verifyhole.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-def verifyhole():
-    
-    return None
